005b94a66e9d0b127f53cd99dbbbd3b20cc27251
CesarLeonelBrito <cesarleonel.brito@gmail.com>
2021-01-29 14:02:26 -0600

    Llenar la clase

--- Lab2_CesarBrito/src/lab2_cesarbrito/Lab2_CesarBrito.java
+++ Lab2_CesarBrito/src/lab2_cesarbrito/Lab2_CesarBrito.java
@@ -1,5 +1,6 @@
 package lab2_cesarbrito;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab2_CesarBrito {
@@ -7,13 +8,13 @@ public class Lab2_CesarBrito {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        ArrayList Restaurantes = new ArrayList();
 
         System.out.println("===========Login===========");
         System.out.print("Ingrese su usuario: ");
         String user = sc.next();
         System.out.print("Ingrese su contraseña: ");
         String pass = sc.next();
-        System.out.println(user + "   " + pass);
         while (!user.equals("david") || !pass.equals("99")) {
             System.out.println("El nombre o la contraseña esta incorrecta, trate de nuevo");
             System.out.print("Ingrese su usuario: ");
@@ -21,7 +22,20 @@ public class Lab2_CesarBrito {
             System.out.print("Ingrese su contraseña: ");
             pass = sc.next();
         }
-        System.out.println("funciona");
+        int opcion = 0;
+        while (opcion != 3) {
+            System.out.println("1- Reporte de restaurantes\n"
+                    + "2- Manejo de restaurantes\n"
+                    + "3- Salir");
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+            }
+        }
 
     }
 
--- Lab2_CesarBrito/src/lab2_cesarbrito/Restaurante.java
+++ Lab2_CesarBrito/src/lab2_cesarbrito/Restaurante.java
@@ -1,14 +1,127 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_cesarbrito;
 
-/**
- *
- * @author cesar
- */
 public class Restaurante {
-    
+
+    private String franquicia;
+    private String nombre;
+    private String ubicacion;
+    private int empleados;
+    private String parqueo;
+    private String juegos;
+    private int mesas;
+    private int cajeros;
+    private String gerente;
+    private String especialidad;
+    private String estado;
+
+    public Restaurante() {
+    }
+
+    public Restaurante(String franquicia, String nombre, String ubicacion, int empleados, String parqueo, String juegos, int mesas, int cajeros, String gerente, String especialidad, String estado) {
+        this.franquicia = franquicia;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.empleados = empleados;
+        this.parqueo = parqueo;
+        this.juegos = juegos;
+        this.mesas = mesas;
+        this.cajeros = cajeros;
+        this.gerente = gerente;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+
+    public String getFranquicia() {
+        return franquicia;
+    }
+
+    public void setFranquicia(String franquicia) {
+        this.franquicia = franquicia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(int empleados) {
+        this.empleados = empleados;
+    }
+
+    public String getParqueo() {
+        return parqueo;
+    }
+
+    public void setParqueo(String parqueo) {
+        this.parqueo = parqueo;
+    }
+
+    public String getJuegos() {
+        return juegos;
+    }
+
+    public void setJuegos(String juegos) {
+        this.juegos = juegos;
+    }
+
+    public int getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(int mesas) {
+        this.mesas = mesas;
+    }
+
+    public int getCajeros() {
+        return cajeros;
+    }
+
+    public void setCajeros(int cajeros) {
+        this.cajeros = cajeros;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurante{" + "franquicia=" + franquicia + ", nombre=" + nombre + ", ubicacion=" + ubicacion + ", empleados=" + empleados + ", parqueo=" + parqueo + ", juegos=" + juegos + ", mesas=" + mesas + ", cajeros=" + cajeros + ", gerente=" + gerente + ", especialidad=" + especialidad + ", estado=" + estado + '}';
+    }
+
 }
